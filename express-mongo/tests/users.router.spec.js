c8ad7b57c954cf11771a263bffd1d59bf2182a3f
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-08-08 14:03:28 +0530

    Update users.router.spec.js

--- express-mongo/tests/users.router.spec.js
+++ express-mongo/tests/users.router.spec.js
@@ -14,47 +14,47 @@ let boundaryTestName = `UsersRoutes boundary test`
 let functionalTestName = `UsersRoutes functional test`
 
 describe("UsersRouter", () => {
+    describe("boundary", () => {
 
-    let users;
+        let users;
 
-    beforeEach(() => {
-        users = [...usersData];
-    })
-    afterEach(() => {
-        users = [];
-    })
+        beforeEach(() => {
+            users = [...usersData];
+        })
+        afterEach(() => {
+            users = [];
+        })
 
-    it(`${boundaryTestName} should get all users`, (done) => {
-        request(app)
-            .get("/")
-            .expect("Content-Type", /application\/json/)
-            .expect({ success: true, data: users })
-            .expect(200, done);
+        it(`${boundaryTestName} should get all users`, (done) => {
+            request(app)
+                .get("/")
+                .expect("Content-Type", /application\/json/)
+                .expect({ success: true, data: users })
+                .expect(200, done);
+        });
+        it(`${boundaryTestName} should create user`, (done) => {
+            let data = { "name": "demo", "password": "demo", "role": "user" };
+
+            request(app)
+                .post("/")
+                .send(data)
+                .expect("Content-Type", /application\/json/)
+                .expect({ success: true })
+                .expect(200, done);
+        });
+
+        it(`${functionalTestName} should not create same user again`, (done) => {
+            request(app)
+                .post("/")
+                .send({ "name": "demo", "password": "demo", "role": "user" })
+                .expect("Content-Type", /application\/json/)
+                .then(() => {
+                    request(app)
+                        .post("/")
+                        .send({ "name": "demo", "password": "demo", "role": "user" })
+                        .expect({ success: false, message: `username is already used` })
+                        .expect(200, done);
+                });
+        });
     });
-    it(`${boundaryTestName} should create user`, (done) => {
-        let data = { "name": "demo", "password": "demo", "role": "user" };
-
-        request(app)
-            .post("/")
-            .send(data)
-            .expect("Content-Type", /application\/json/)
-            .expect({ success: true })
-            .expect(200, done);
-    });
-
-    it(`${functionalTestName} should not create same user again`, (done) => {
-        request(app)
-            .post("/")
-            .send({ "name": "demo", "password": "demo", "role": "user" })
-            .expect("Content-Type", /application\/json/)
-            .then(() => {
-                request(app)
-                    .post("/")
-                    .send({ "name": "demo", "password": "demo", "role": "user" })
-                    .expect({ success: false, message: `username is already used` })
-                    .expect(200, done);
-            });
-
-    });
-
-})
+});
