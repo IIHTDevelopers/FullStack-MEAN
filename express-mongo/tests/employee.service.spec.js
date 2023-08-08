f174785ba119f97fbf47616a0252797d9a2b47c7
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-08-08 14:03:07 +0530

    Update employee.service.spec.js

--- express-mongo/tests/employee.service.spec.js
+++ express-mongo/tests/employee.service.spec.js
@@ -6,41 +6,42 @@ let boundaryTestName = `EmployeeServcie boundary test`
 let functionalTestName = `EmployeeServcie functional test`
 
 describe('EmployeeServcie', () => {
-    it(`${boundaryTestName} should find employee by id`, (done) => {
-        const employee = {
-            _id: '62f8dfad306e17471c7f7ee7',
-            name: 'Tom',
-            address: 'tokyo, japan',
-            salary: 10000
-        };
+    describe('exception', () => {
+        it(`${boundaryTestName} should find employee by id`, (done) => {
+            const employee = {
+                _id: '62f8dfad306e17471c7f7ee7',
+                name: 'Tom',
+                address: 'tokyo, japan',
+                salary: 10000
+            };
 
-        mockingoose(EmployeeModel).toReturn(employee, 'findOne');// findById is findOne in mokingoose
+            mockingoose(EmployeeModel).toReturn(employee, 'findOne');// findById is findOne in mokingoose
 
-        employeeService.findById(employee._id, (err, data) => {
-            expect(!!err).toBe(false);
-            expect(!!data).toBe(true);
-            expect(data.name).toBe(employee.name);
-            expect(data.address).toBe(employee.address);
-            expect(data.salary).toBe(employee.salary);
-            let today = new Date();
-            expect(new Date(data.joining_date).getDate()).toBe(today.getDate());
-            expect(new Date(data.joining_date).getMonth()).toBe(today.getMonth());
-            expect(new Date(data.joining_date).getFullYear()).toBe(today.getFullYear());
-            done();
+            employeeService.findById(employee._id, (err, data) => {
+                expect(!!err).toBe(false);
+                expect(!!data).toBe(true);
+                expect(data.name).toBe(employee.name);
+                expect(data.address).toBe(employee.address);
+                expect(data.salary).toBe(employee.salary);
+                let today = new Date();
+                expect(new Date(data.joining_date).getDate()).toBe(today.getDate());
+                expect(new Date(data.joining_date).getMonth()).toBe(today.getMonth());
+                expect(new Date(data.joining_date).getFullYear()).toBe(today.getFullYear());
+                done();
+            });
+        });
+
+        it(`${functionalTestName} should not find unsaved employee`, (done) => {
+
+            const employee = null;
+
+            mockingoose(EmployeeModel).toReturn(employee, 'findOne');// findById is findOne in mokingoose
+
+            employeeService.findById("62f7ff5d5e6af8151e15ae29", (err, data) => {
+                expect(!!err).toBe(false);
+                expect(data).toBeNull();
+                done();
+            });
         });
     });
-
-    it(`${functionalTestName} should not find unsaved employee`, (done) => {
-
-        const employee = null;
-
-        mockingoose(EmployeeModel).toReturn(employee, 'findOne');// findById is findOne in mokingoose
-
-        employeeService.findById("62f7ff5d5e6af8151e15ae29", (err, data) => {
-            expect(!!err).toBe(false);
-            expect(data).toBeNull();
-            done();
-        });
-    });
-
 });
